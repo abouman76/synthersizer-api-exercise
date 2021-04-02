a433f5da822a9165132af9ea1879b839992204a6
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-04-02 13:52:47 +0200

    fetching data first 20 synthersizers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [synthersizers, setSynthersizers] = useState([]);
+
+  // const synthersizerName = synthersizers.map((synthersizer) => {
+  //   console.log("1 SYNTH?", synthersizer);
+  //   return <li className="style-card">{synthersizer.name}</li>;
+  // });
+
+  useEffect(() => {
+    async function fetchSynthersizer() {
+      // console.log("fetching");
+      try {
+        const { data } = await axios.get(
+          `https://synthesizer-api.herokuapp.com/api/synths?key=T60ZPHZ-D8V4Z93-MNW77EK-A4DN790&yearProduced=1980`
+        );
+        // console.log("RESPONSE", data.synths);
+        setSynthersizers(data.synths);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchSynthersizer();
+  }, []);
+  // console.log("synth", synthersizers[0]?.name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-card">
+        {synthersizers.map((synthersizer) => {
+          console.log("1 SYNTH?", synthersizer);
+          // console.log("manu", synthersizers);
+          return (
+            <ul key={synthersizer.id} className="style-card">
+              <li>
+                <h2>{synthersizer.name.toUpperCase()}</h2>
+              </li>
+              <img className="img" src={synthersizer.img} alt="synthersizer" />
+              <li>Manufacturer: {synthersizer.Manufacturer?.manufacturer}</li>
+            </ul>
+          );
+        })}
+      </div>
+    </>
   );
 }
-
 export default App;

58fca94569faa2eec010f691dcb0959f26fdf8d0
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-04-02 14:58:57 +0200

    pagination

--- src/App.js
+++ src/App.js
@@ -4,27 +4,34 @@ import "./App.css";
 
 function App() {
   const [synthesizers, setSynthesizers] = useState([]);
+  const [page, setPage] = useState(0);
 
-  // const synthersizerName = synthersizers.map((synthersizer) => {
-  //   console.log("1 SYNTH?", synthersizer);
-  //   return <li className="style-card">{synthersizer.name}</li>;
-  // });
+  const showPreviousPage = () => {
+    setPage(page - 1);
+  };
 
+  const showNextPage = () => {
+    setPage(page + 1);
+  };
+  console.log("page", page);
   useEffect(() => {
     async function fetchSynthesizer() {
       // console.log("fetching");
       try {
         const { data } = await axios.get(
-          `https://synthesizer-api.herokuapp.com/api/synths?key=T60ZPHZ-D8V4Z93-MNW77EK-A4DN790&yearProduced=1980`
+          `https://synthesizer-api.herokuapp.com/api/synths?key=T60ZPHZ-D8V4Z93-MNW77EK-A4DN790&limit=20&offset=${page}`
         );
         // console.log("RESPONSE", data.synths);
         setSynthesizers(data.synths);
+        // setNextPage();
+        // setPreviousPage();
       } catch (error) {
         console.error(error);
       }
     }
     fetchSynthesizer();
-  }, []);
+  }, [page]);
+
   // console.log("synth", synthersizers[0]?.name);
 
   return (
@@ -35,17 +42,16 @@ function App() {
       </div>
 
       <div className="buttons">
-        <button className="btn" type="button" onClick={() => alert("Previous")}>
+        <button className="btn" type="button" onClick={showPreviousPage}>
           Previous
         </button>
-        <button className="btn" type="button" onClick={() => alert("Next")}>
+        <button className="btn" type="button" onClick={showNextPage}>
           Next
         </button>
       </div>
       <div className="main-card">
         {synthesizers.map((synthesizer) => {
-          console.log("1 SYNTH?", synthesizer);
-          // console.log("manu", synthersizers);
+          // console.log("1 SYNTH?", synthesizer);
           return (
             <ul key={synthesizer.id} className="style-card">
               <li>
---
+++ src/SynthersizerCard.js
@@ -0,0 +1,3 @@
+import Reacht from "react";
+
+export default SynthersizerCard.js;

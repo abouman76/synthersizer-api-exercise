3f796dd3f4f4eb4533791c24bea10dd526e5f3e8
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-04-02 14:20:52 +0200

    spelling check

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [synthersizers, setSynthersizers] = useState([]);
+  const [synthesizers, setSynthesizers] = useState([]);
 
   // const synthersizerName = synthersizers.map((synthersizer) => {
   //   console.log("1 SYNTH?", synthersizer);
@@ -11,27 +11,27 @@ function App() {
   // });
 
   useEffect(() => {
-    async function fetchSynthersizer() {
+    async function fetchSynthesizer() {
       // console.log("fetching");
       try {
         const { data } = await axios.get(
           `https://synthesizer-api.herokuapp.com/api/synths?key=T60ZPHZ-D8V4Z93-MNW77EK-A4DN790&yearProduced=1980`
         );
         // console.log("RESPONSE", data.synths);
-        setSynthersizers(data.synths);
+        setSynthesizers(data.synths);
       } catch (error) {
         console.error(error);
       }
     }
-    fetchSynthersizer();
+    fetchSynthesizer();
   }, []);
   // console.log("synth", synthersizers[0]?.name);
 
   return (
     <>
       <div className="head-text">
-        <h1>Let's grab some synthersizer date!</h1>
-        <h4>Data from Bernard's Synthersizer API</h4>
+        <h1>Let's grab some synthesizer data!</h1>
+        <h4>Data from Bernard's Synthesizer API</h4>
       </div>
 
       <div className="buttons">
@@ -43,16 +43,16 @@ function App() {
         </button>
       </div>
       <div className="main-card">
-        {synthersizers.map((synthersizer) => {
-          console.log("1 SYNTH?", synthersizer);
+        {synthesizers.map((synthesizer) => {
+          console.log("1 SYNTH?", synthesizer);
           // console.log("manu", synthersizers);
           return (
-            <ul key={synthersizer.id} className="style-card">
+            <ul key={synthesizer.id} className="style-card">
               <li>
-                <h2>{synthersizer.name.toUpperCase()}</h2>
+                <h2>{synthesizer.name.toUpperCase()}</h2>
               </li>
-              <img className="img" src={synthersizer.img} alt="synthersizer" />
-              <li>Manufacturer: {synthersizer.Manufacturer?.manufacturer}</li>
+              <img className="img" src={synthesizer.img} alt="synthesizer" />
+              <li>Manufacturer: {synthesizer.Manufacturer?.manufacturer}</li>
             </ul>
           );
         })}
